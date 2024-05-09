ec100bede0e758fb0b63927331f1c1bf12ef2361
JeongHyun Ha <aszx2024@naver.com>
2024-05-09 14:43:28 +0900

    쓸데없는 코드 지우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,6 @@
-function App() {
-  // javascript data type
-  let a; // undefined
-  console.log("a", typeof a);
-  let b = "문자열 타입";
-  console.log("b", typeof b);
-  let c = 3; // number
-  let d = 3.14; // number
-  console.log("c", typeof c);
-  console.log("d", typeof d);
-  let e = true; // boolean
-  console.log("e", typeof e);
-  let f = {}; // object
-  console.log("f", typeof f);
-  let g = function () {}; // function
-  console.log("g", typeof g);
+import React from "react";
+
+function App(props) {
   return <div></div>;
 }
 
