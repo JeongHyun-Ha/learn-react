bb84336bc11c888a1faed811ac8a3bbc27735907
JeongHyun Ha <aszx2024@naver.com>
2024-05-09 16:24:39 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,7 @@
 import React from "react";
 
 function App(props) {
-  const a = ["흥민", "강인", "희찬"];
-  const b = a.map(function (item) {
-    return item + "선수";
-  });
-
-  const c = a.map(function (item) {
-    return <li>{item}</li>;
-  });
-
-  return (
-    <>
-      <div>{a}</div>
-      <div>
-        {a[0]}
-        {a[1]}
-        {a[2]}
-      </div>
-      <div>{b}</div>
-      <ul>{c}</ul>
-      <div>
-        {a.map(function (item) {
-          return <li>{item} 선수</li>;
-        })}
-      </div>
-      <div>
-        {a.map((item) => (
-          <li>{item} 선수</li>
-        ))}
-      </div>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
