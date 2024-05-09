a39055d9dfa1a362e765ea93eb834f3a942bbb9e
JeongHyun Ha <aszx2024@naver.com>
2024-05-09 11:22:13 +0900

    최상이 컴포넌트 1개, Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+function MyComp2() {
+  // 최상위 컴포넌트를 사용하지 않을 때 -> fragment 사용 <></>
+  return (
+    <>
+      <p>Lorem ipsum dolor.</p>
+      <p>Accusantium enim, obcaecati.</p>
+    </>
+  );
+}
+
+function MyComp() {
+  // 최상위 컴포넌트는 1개임
+  return (
+    <div>
+      <p>Lorem ipsum.</p>
+      <p>Consectetur, provident!</p>
+    </div>
+  );
+}
+
+function MyTag() {
+  // jsx 코드가 여러 줄일 때 () 사용
+  return (
+    <div>
+      hello tag
+      <p>Lorem ipsum.</p>
+      <p>Architecto, voluptatem!</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <>
-      <div>hello react</div>
+      <h1>hello react</h1>
+      <MyTag />
+      <MyComp />
+      <MyComp2 />
     </>
   );
 }
