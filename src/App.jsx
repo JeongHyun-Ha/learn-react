f8ccbf0da04d09330cb351fd69f90c453520009f
JeongHyun Ha <aszx2024@naver.com>
2024-05-09 15:38:11 +0900

    필요없는 코드 지우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
 import React from "react";
 
 function App(props) {
-  const a = {};
-  console.log("a", typeof a);
-
-  const b = [];
-  console.log("b", typeof b);
-
-  // 배열: 여러 값을 순서(index)대로 가지고 있는 객체
-  const c = ["son", "lee", "kim", "hwang"];
-  console.log("c", c);
-  console.log("c[0]", c[0]);
-  console.log("c[1]", c[1]);
-  console.log("c[2]", c[2]);
-  console.log("c[3]", c[3]);
-  console.log("c[4]", c[4]); // 없는 index
-
-  return (
-    <ul>
-      <li>{c[0]}</li>
-      <li>{c[1]}</li>
-      <li>{c[2]}</li>
-      <li>{c[3]}</li>
-      <li>{c[4]}</li>
-    </ul>
-  );
+  return <div></div>;
 }
 
 export default App;
