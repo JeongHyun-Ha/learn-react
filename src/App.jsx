5b59de1764d7910309a21633f3861d9e7e599afe
JeongHyun Ha <aszx2024@naver.com>
2024-05-09 15:31:26 +0900

    필요없는 코드 지우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
 import React from "react";
 
 function App(props) {
-  // object
-  const a = {
-    name: "John",
-  };
-
-  const b = a;
-  console.log("a.name", a.name);
-
-  b.name = "Jane";
-
-  console.log("b.name", b.name);
-  console.log("a.name", a.name);
   return <div></div>;
 }
 
