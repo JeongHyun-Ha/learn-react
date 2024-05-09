5f197d00db5e0f7bc71faec564c6fd6beb92d82e
JeongHyun Ha <aszx2024@naver.com>
2024-05-09 14:33:34 +0900

    falsy, truthy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
 function App() {
+  // falsy, truthy
+  // falsy(false 같은)
+  //  : false, 0, "", null, undefined
+
+  const a = 0;
+  if (a) {
+    console.log("a는 참이다.");
+  } else {
+    console.log("a는 거짓이다");
+  }
+  const b = 1;
+  let c = b && "값이 있음";
+  let d = b && "값이 없음";
+  console.log("c", c);
+  console.log("d", d);
+
   return <div></div>;
 }
 
