600b637caf35548ec70a7c0691301af24ddbc8dc
JeongHyun Ha <aszx2024@naver.com>
2024-05-09 15:07:53 +0900

    javascript : object의 property

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 
 function App(props) {
+  // object
+  // {} 안에 property 명 : property 값 쌍들이 작성됨
+  let a = { name: "흥민", age: 30, address: "korea" };
+  let b = {};
+  console.log("a", a);
+  console.log("b", b);
+
+  console.log("a.name ", a.name);
+  console.log("a.age", a["age"]);
+
+  b.name = "강인";
+  console.log("b", b);
+  b.name = "이강인";
+  console.log("b", b);
+
+  // 없는 프로퍼티에도 접근 가능
+  console.log("b의 city", b.city);
+  b.city = "부산";
+  b.address = "강남";
+  console.log("b", b);
   return <div></div>;
 }
 
