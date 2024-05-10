5b354c19ad884059f45a68f77e5ef28a62fe2544
JeongHyun Ha <aszx2024@naver.com>
2024-05-10 10:19:39 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
 import React from "react";
 
 function App(props) {
-  // 이름있는 함수
-  function action1() {
-    console.log("Action 1");
-  }
-
-  // 이름 없는 함수
-  const a = function () {
-    console.log("anonymous action");
-  };
-  return (
-    <div>
-      <button onClick={action1}>클릭!</button>
-      <br />
-      <button onClick={a}>클릭@</button>
-      <br />
-      <button onClick={() => console.log("arrow function")}>클릭$</button>
-      <br />
-      <button
-        onClick={function () {
-          console.log("annonymous function!!");
-        }}
-      >
-        클릭#
-      </button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
