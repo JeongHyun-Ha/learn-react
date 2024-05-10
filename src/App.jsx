2d53319d3b87b8dc7b733f2c0b092adc27fe7a6c
JeongHyun Ha <aszx2024@naver.com>
2024-05-10 15:11:14 +0900

    props의 값타입은 javascript 자료형
    값은 {} 안에 작성
    단, string 이면 {}생략 가능

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-function App(props) {
+function MyComp(props) {
   return <div></div>;
+}
+
+function App(props) {
+  return (
+    <div>
+      <MyComp
+        name="son" // string type 은 중괄호 생략 가능
+        // name={"son"}
+        age={3.14}
+        address={{ city: "seoul", country: "us" }}
+        foods={["pizza", "coffee"]}
+        married={true}
+        action={function () {
+          console.log("action");
+        }}
+        someAction={() => console.log("someAction")}
+      />
+    </div>
+  );
 }
 
 export default App;
