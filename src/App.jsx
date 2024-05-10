bf3650a6f1f0ad26ee3606cbee7d8f9f02bf0870
JeongHyun Ha <aszx2024@naver.com>
2024-05-10 15:23:34 +0900

    props에 값이 없이 이름만 작성되면 값이 true

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 
-function MyComp(props) {
+function MyComp({ name, age, someProp, otherProp }) {
+  console.log("someProp", someProp); // true
+  console.log("otherProp", otherProp); // undefined
   return <div></div>;
 }
 
 function App(props) {
-  // props의 이름은 lowerCamelCase 로 작성
-  // 예약어를 쓰지 말 것
-
   return (
     <div>
-      <label htmlFor="input1">aaa</label>
-      <input id="input1" type="text" />
-      <p className="error note"></p>
-      <MyComp number={3} pageNumber={3} let={5} const={5} />
+      <MyComp name="son" age={30} someProp={true} />
     </div>
   );
 }
