b4a50c45693a1bbae3021e72097a06fe6dcba2c1
JeongHyun Ha <aszx2024@naver.com>
2024-05-10 14:13:53 +0900

    props(properties)

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+function MyButton(props) {
+  return (
+    <button
+      style={{
+        padding: "10px",
+        backgroundColor: "green",
+        color: "white",
+      }}
+    >
+      {props.name}
+    </button>
+  );
+}
+
+function App(props) {
+  return (
+    <div>
+      <button
+        style={{
+          padding: "10px",
+          backgroundColor: "blue",
+          color: "white",
+        }}
+      >
+        클릭1
+      </button>
+      <br />
+      <button
+        style={{
+          padding: "10px",
+          backgroundColor: "blue",
+          color: "white",
+        }}
+      >
+        클릭2
+      </button>
+      <br />
+      <MyButton name="클릭3" />
+      <br />
+      <MyButton name="클릭4" />
+    </div>
+  );
+}
+
+export default App;
