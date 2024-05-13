898e7a7ddb938596907a59c91e141d889da04354
JeongHyun Ha <aszx2024@naver.com>
2024-05-13 11:11:47 +0900

    default export

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+// default export는 아무 이름으로 import 할 수 있음, 중괄호 없이
+import val, { v1 } from "./MyValues.jsx";
+import str from "./MyComps.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>{v1}</div>
+      <div>{val}</div>
+      <div>{str}</div>
+    </div>
+  );
 }
 
 export default App;
--- src/MyComps.jsx
+++ src/MyComps.jsx
@@ -1,4 +1,2 @@
-const v1 = "korea";
-const v2 = 44;
-
-export { v1, v2 };
+const val = "hello";
+export default val;
