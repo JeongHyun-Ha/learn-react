ed7d06b50ad75ee326d0fa41e3471da8c9875335
JeongHyun Ha <aszx2024@naver.com>
2024-05-13 16:43:11 +0900

    상태 예제 7
    하위 컴포넌트의 상태와 이벤트는 상위 컴포넌트에서 관리할 수 있다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyButton({ onClick }) {
+  return <button onClick={onClick}>클릭</button>;
+}
+
+function MyNumber({ number }) {
+  return <div>number : {number}</div>;
+}
 
 function App(props) {
-  return <div></div>;
+  const [number, setNumber] = useState(0);
+
+  function handleNumberChange() {
+    setNumber(number + 1);
+  }
+
+  // 하위 컴포넌트의 상태와 이벤트는 상위 컴포넌트에서 관리할 수 있다.
+  return (
+    <div>
+      <MyButton onClick={handleNumberChange} />
+      <MyNumber number={number} />
+    </div>
+  );
 }
 
 export default App;
