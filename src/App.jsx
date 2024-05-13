c37f52623035673957030c70aa6b98e1786b538d
JeongHyun Ha <aszx2024@naver.com>
2024-05-13 09:48:57 +0900

    import, export

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+// import : 다른 파일에 있는 변수를 현재 파일에서 사용
+import { value1, value2 } from "./MyValues.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        value1 : {value1}
+        value2 : {value2}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/MyValues.jsx
@@ -0,0 +1,8 @@
+// export : 다른 파일에서 사용가능하도록 내보내기
+const value1 = "son";
+
+const value2 = "lee";
+
+const value3 = 44;
+
+const value4 = { name: "kim", team: "뮌헨" };
