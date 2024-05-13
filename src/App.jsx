4213ea0ebe87caeb177f05cd222c454dd01e6387
JeongHyun Ha <aszx2024@naver.com>
2024-05-13 12:36:29 +0900

    한 컴포넌트가 여러 상태를 가질 수 있음

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp() {
+  const [val1, setVal1] = useState(0);
+  const [val2, setVal2] = useState(0);
+
+  return (
+    <div>
+      <div>{val1}</div>
+      <div>
+        <button onClick={() => setVal1(val1 + 1)}>click1</button>
+      </div>
+      <div>{val2}</div>
+      <div>
+        <button onClick={() => setVal2(val2 + 2)}>click2</button>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
