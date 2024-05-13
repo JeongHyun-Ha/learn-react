e94c2c89f1d1b3e72489286f81bc998c6d6b3b84
JeongHyun Ha <aszx2024@naver.com>
2024-05-13 15:17:34 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [numList, setNumList] = useState([]);
-  const [text, setText] = useState("");
-
-  function handleAddList() {
-    numList.push(text);
-    const [...newList] = numList;
-    setNumList(newList);
-  }
-
-  return (
-    <div>
-      <input type="text" onChange={(e) => setText(e.target.value)} />
-      <button onClick={handleAddList}>추가</button>
-      <ul>
-        {numList.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
