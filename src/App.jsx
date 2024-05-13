27d7242332e9c939966cda89719546a00377f31e
JeongHyun Ha <aszx2024@naver.com>
2024-05-13 15:42:01 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [team, setTeam] = useState("");
-  const [country, setCountry] = useState("");
-
-  return (
-    <div>
-      <div>
-        이름
-        <input
-          type="text"
-          placeholder={"이름"}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div>
-        나이
-        <input
-          type="text"
-          placeholder={"나이"}
-          onChange={(e) => setAge(e.target.value)}
-        />
-      </div>
-      <div>
-        팀
-        <input
-          type="text"
-          placeholder={"팀"}
-          onChange={(e) => setTeam(e.target.value)}
-        />
-      </div>
-      <div>
-        국가
-        <input
-          type="text"
-          placeholder={"국가"}
-          onChange={(e) => setCountry(e.target.value)}
-        />
-      </div>
-      <div>
-        {age}세 {name}은 {country}, {team}소속입니다.
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
