8c6ba8b7935941d5716fb73c1df244aad7c48b4f
JeongHyun Ha <aszx2024@naver.com>
2024-05-13 16:16:08 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [person, setPerson] = useState({
-    name: "",
-    address: "",
-    city: "",
-    email: "",
-  });
-
-  return (
-    <div>
-      <div>
-        <input
-          type="text"
-          placeholder={"이름"}
-          onChange={(event) => {
-            const { ...newPerson } = person;
-            newPerson.name = event.target.value;
-            setPerson(newPerson);
-          }}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder={"이메일"}
-          onChange={(event) => {
-            const { ...newPerson } = person;
-            newPerson.email = event.target.value;
-            setPerson(newPerson);
-          }}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder={"도시"}
-          onChange={(event) => {
-            const newPerson = { ...person, city: event.target.value };
-            setPerson(newPerson);
-          }}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder={"주소"}
-          onChange={(event) => {
-            const newPerson = { ...person, address: event.target.value };
-            setPerson(newPerson);
-          }}
-        />
-      </div>
-      <ul>
-        <li>name: {person.name}</li>
-        <li>email: {person.email}</li>
-        <li>city: {person.city}</li>
-        <li>address: {person.address}</li>
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
