2468e87637f63dff4d49583b28ff9cad5405e911
JeongHyun Ha <aszx2024@naver.com>
2024-05-13 12:19:30 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp1() {
-  const [val, setVal] = useState([]);
-  function addItem() {
-    val.push("a");
-    setVal(val);
-  }
-  return (
-    <div>
-      <div>{val}</div>
-      <div>
-        <button onClick={addItem}>변경</button>
-      </div>
-    </div>
-  );
-}
-
-function MyComp2() {
-  const [val, setVal] = useState([]);
-  function addItem() {
-    const [...newVal] = val;
-    newVal.push("a");
-    setVal(newVal);
-  }
-  return (
-    <div>
-      <div>{val}</div>
-      <div>
-        <button onClick={addItem}>변경</button>
-      </div>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp1 />
-      <MyComp2 />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
