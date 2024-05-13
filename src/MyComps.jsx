003c06d1cd6235dbc881df6d5af5d3eaef166904
JeongHyun Ha <aszx2024@naver.com>
2024-05-13 10:32:27 +0900

    react component를 만드는 방법
    이름 있는 함수
    이름 없는 함수
    arrow function

---
+++ src/MyComps.jsx
@@ -0,0 +1,12 @@
+// 이름 있는 함수
+export function MyComp() {
+  return <div>myComp</div>;
+}
+
+// 이름 있는 함수
+export const YourComp = function () {
+  return <div>yourComp</div>;
+};
+
+// arrow
+export const ArrowComp = () => <div>arrow function component</div>;
