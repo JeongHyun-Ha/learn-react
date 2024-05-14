4fccf5d20fd2e7f5cd9cb786255452c77ef0e92b
JeongHyun Ha <aszx2024@naver.com>
2024-05-14 12:26:22 +0900

    컨텍스트로 데이터(객체) 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
+
+let TextContext = createContext(null);
+
+function MyInput() {
+  const textHandler = useContext(TextContext);
+  return (
+    <div>
+      <input type="text" onChange={textHandler.setText} />
+      <p>MyInput : {textHandler.text}</p>
+    </div>
+  );
+}
+
+function MyText() {
+  const textHandler = useContext(TextContext);
+  return (
+    <div>
+      <p>MyText : {textHandler.text}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+
+  function handleUpdateText(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <div>
+      <TextContext.Provider value={{ text, setText: handleUpdateText }}>
+        <MyInput />
+        <MyText />
+      </TextContext.Provider>
+    </div>
+  );
 }
 
 export default App;
