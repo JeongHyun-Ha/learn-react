b99a446dc7ac936635a7f77ddef4f35fd9c1aed8
JeongHyun Ha <aszx2024@naver.com>
2024-05-14 14:46:09 +0900

    useEffect()
    4. 두번째 파라미터로 dependency 결정
    
    dependency : 첫번째 파라미터를 trigger하는 값들

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function MyComp() {
   // 두번째 파라미터가 없을 때
   // rendering 될 때마다 실행
   useEffect(() => {
-    console.log("use effect 의 함수 실행됨");
+    console.log("rendering될 때 마다 실행");
   });
 
   // 두번째 파라미터에 빈 배열을 넣으면
@@ -26,6 +26,20 @@ function MyComp() {
     return () => console.log("unmount 때만 실행되는 메소드");
   }, []);
 
+  // 두번째 파라미터(배열)의 원소
+  // 어떤 값(주로 state, dependency)이 바뀔 때
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("");
+  useEffect(() => {
+    console.log("text1: dependency가 바뀔 때");
+  }, [text1]);
+  useEffect(() => {
+    console.log("text2: dependency가 바뀔 때");
+  }, [text2]);
+  useEffect(() => {
+    console.log("text1 또는 text2가 바뀔 때");
+  }, [text1, text2]);
+
   const [count, setCount] = useState(0);
 
   return (
@@ -33,6 +47,11 @@ function MyComp() {
       hello comp
       <p>{count}</p>
       <button onClick={() => setCount(count + 1)}>Click</button>
+      <hr />
+      <div>
+        <input type="text" onChange={(e) => setText1(e.target.value)} />
+        <input type="text" onChange={(e) => setText2(e.target.value)} />
+      </div>
     </div>
   );
 }
