6c8525572b11d3a27a13c17faf44004fe85c3699
JeongHyun Ha <aszx2024@naver.com>
2024-05-14 09:13:15 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-import React, { useState } from "react";
-
-function MyButton({ onClick }) {
-  return <button onClick={onClick}>클릭</button>;
-}
-
-function MyNumber({ number }) {
-  return <div>number : {number}</div>;
-}
+import React from "react";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
-
-  function handleNumberChange() {
-    setNumber(number + 1);
-  }
-
-  // 하위 컴포넌트의 상태와 이벤트는 상위 컴포넌트에서 관리할 수 있다.
-  return (
-    <div>
-      <MyButton onClick={handleNumberChange} />
-      <MyNumber number={number} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
