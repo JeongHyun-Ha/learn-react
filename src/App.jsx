7e020d3970b455c5f4b0fb69442d926f67b9ae4b
JeongHyun Ha <aszx2024@naver.com>
2024-05-14 15:10:37 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("mount 될 때");
-  }, []);
-  useEffect(() => {
-    console.log("count 가 변경될 때");
-    // setCount(count + 1); // 무한 루프
-  }, [count]);
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>Click</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
