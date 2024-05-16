85e8e3b00e1e8d9965574d9caf02e6deaed57123
JeongHyun Ha <aszx2024@naver.com>
2024-05-16 14:03:22 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,7 @@
 import React from "react";
-import axios from "axios";
 
 function App(props) {
-  function handleClick1() {
-    const data = {
-      name: "손흥민",
-      age: 33,
-      married: false,
-      team: {
-        location: "london",
-        name: "토트넘",
-      },
-      item: ["phone", "shoes", "shirts"],
-      address: null,
-    };
-    axios.post("/api/main41/sub5", data);
-  }
-
-  function handleClick2() {
-    axios.post("/api/main41/sub6", {
-      name: "son",
-      age: 33,
-    });
-  }
-
-  function handleClick3() {
-    axios.post("/api/main41/sub7", {
-      name: "hello",
-      age: 100,
-    });
-  }
-
-  return (
-    <div>
-      <button onClick={handleClick1}>json 데이터와 요청</button>
-      <button onClick={handleClick2}>json 데이터와 요청</button>
-      <hr />
-      <div>
-        <button onClick={handleClick3}>json 데이터와 요청</button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
