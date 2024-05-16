43d2224959d4f0be504a5cd892445a6e82714edb
JeongHyun Ha <aszx2024@naver.com>
2024-05-16 09:22:07 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 function App(props) {
-  const [customerId, setCustomerId] = useState("");
-  const [customerName, setCustomerName] = useState("");
-
-  function handleSearchClick() {
-    // 조회버튼 클릭 시
-    // /api/customer?id=3 로 요청
-    axios
-      .get(`/api/customer?id=${customerId}`)
-      .then((response) => setCustomerName(response.data));
-  }
-
-  return (
-    <div>
-      <div>
-        <input type="number" onChange={(e) => setCustomerId(e.target.value)} />
-        <button onClick={handleSearchClick}>조회</button>
-      </div>
-      <div>이름 : {customerName}</div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
