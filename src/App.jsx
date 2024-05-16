ffcccc7cd2da386a9371052c3f246131478054a2
JeongHyun Ha <aszx2024@naver.com>
2024-05-16 15:39:31 +0900

    응답 코드 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
 
 function App(props) {
-  const [result, setResult] = useState("");
-  function handleClick1() {
-    axios
-      .get("/api/main42/sub1")
-      .then((response) => console.log(response.data));
-  }
-
-  function handleClick2() {
-    axios.get("/api/main42/sub2").then((response) => setResult(response.data));
-  }
-
-  function handleClick3() {
-    axios.get("/api/main42/sub3").then((res) => console.log(res.data));
-  }
-
-  function handleClick4() {
-    axios.get("/api/main42/sub4").then((res) => console.log(res.data));
-  }
-
-  function handleClick5() {
-    axios.get("/api/main42/sub5").then((res) => console.log(res.data));
-  }
-
   return (
     <div>
-      <button onClick={handleClick1}>응답 받기</button>
+      <button onClick={() => axios.get("/api/main43/sub1")}>200 응답</button>
       <br />
-      <button onClick={handleClick2}>응답 받기</button>
-      <p>{result}</p>
+      <button onClick={() => axios.get("/api/main43/sub2")}>400 응답</button>
       <hr />
-      <button onClick={handleClick3}>JSON 응답 받기</button>
-      <hr />
-      <button onClick={handleClick4}>JSON 응답 받기2</button>
-      <hr />
-      <button onClick={handleClick5}>JSON 응답 받기3</button>
+      <button onClick={() => axios.get("/api/main43/sub3?code=401")}>
+        401 응답
+      </button>
+      <button onClick={() => axios.get("/api/main43/sub3?code=403")}>
+        403 응답
+      </button>
+      <button onClick={() => axios.get("/api/main43/sub3?code=404")}>
+        404 응답
+      </button>
+      <button onClick={() => axios.get("/api/main43/sub3?code=500")}>
+        500 응답
+      </button>
     </div>
   );
 }
