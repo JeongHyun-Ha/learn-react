39d9c177ec7652706d8938ce830872a0bfb74d67
JeongHyun Ha <aszx2024@naver.com>
2024-05-16 10:26:45 +0900

    axios get, post 요청 with query string

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,40 @@ function App(props) {
     axios.post("/api/someurl");
   }
 
+  function handleClickButton3() {
+    axios.get("/api/someurl2");
+  }
+
+  function handleClickButton4() {
+    axios.post("/api/someurl2");
+  }
+
+  function handleClickButton5() {
+    const params = new URLSearchParams();
+    params.append("id", 3);
+    params.append("name", "son");
+    params.append("email", "son@son.com");
+    axios.get("/api/someurl3?" + params);
+  }
+
+  function handleClickButton6() {
+    const params = new URLSearchParams();
+    params.append("id", 5);
+    params.append("city", "서울");
+    params.append("country", "한국");
+    axios.post("/api/someurl3", params);
+  }
+
   return (
     <div>
       <button onClick={handleClickButton1}>get 요청</button>
       <button onClick={handleClickButton2}>post 요청</button>
+      <hr />
+      <button onClick={handleClickButton3}>get 요청</button>
+      <button onClick={handleClickButton4}>post 요청</button>
+      <hr />
+      <button onClick={handleClickButton5}>get 요청 with query string</button>
+      <button onClick={handleClickButton6}>post 요청 with data</button>
     </div>
   );
 }
