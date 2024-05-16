1ba580de5ad35d690a2a9a91776af7a6a16f4a2d
JeongHyun Ha <aszx2024@naver.com>
2024-05-16 11:11:34 +0900

    JSON, 직렬화 예제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  const obj1 = { name: "son", age: 33 };
+  const json1 = '{ "name": "son", "age": 33 }';
+
+  const obj2 = { name: "lee", age: 22 };
+  const json2 = JSON.stringify(obj2);
+  return (
+    <div>
+      <p>{json2}</p>
+    </div>
+  );
 }
 
 export default App;
