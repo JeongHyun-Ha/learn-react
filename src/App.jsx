e4de0675708b990762c025df5732553d269cb802
JeongHyun Ha <aszx2024@naver.com>
2024-05-16 12:19:40 +0900

    각 타입 데이터를 json 형식으로 보내기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClick1() {
+    const data = {
+      name: "손흥민",
+      age: 33,
+      married: false,
+      team: {
+        location: "london",
+        name: "토트넘",
+      },
+      item: ["phone", "shoes", "shirts"],
+      address: null,
+    };
+    axios.post("/api/main41/sub5", data);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>json 데이터와 요청</button>
+    </div>
+  );
 }
 
 export default App;
