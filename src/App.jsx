2bbae6b0c26c7367ea80d36d3cb99c12adc7073d
JeongHyun Ha <aszx2024@naver.com>
2024-05-16 16:34:14 +0900

    응답 코드
    1. then() 200번대 응답
    2. catch() 그 외 응답
    3. finally() 항상 응답

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,33 @@ import axios from "axios";
 
 function App(props) {
   const [code, setCode] = useState(200);
-  function handleClick200() {
-    axios.get("/api/main43/sub1").then((res) => console.log("200 응답"));
+  function handle200() {
+    axios.get("/api/main43/sub1").then((res) => console.log(res));
   }
 
-  function handleClickCode() {
+  function handleCode() {
     axios
       .get(`/api/main43/sub3?code=${code}`)
-      .catch((res) => console.log(`${code} 응답`));
+      .then((res) => console.log("200번대 응답")) // 200번대 오류코드 응답
+      .catch((err) => {
+        // 200번대 외의 오류 코드 응답
+        if (err.response.status === 401) {
+          console.log("401번 오류 코드");
+        } else if (err.response.status === 403) {
+          console.log("403번 오류 코드");
+        } else {
+          console.log("그 외 오류 코드");
+        }
+      })
+      .finally(() => console.log("항상 실행")); // 항상 실행
   }
 
   return (
     <div>
-      <button onClick={handleClick200}>200 응답</button>
+      <button onClick={handle200}>200 응답</button>
       <hr />
       <input type="number" onChange={(e) => setCode(e.target.value)} />
-      <button onClick={handleClickCode}>{code} 응답</button>
+      <button onClick={handleCode}>응답 코드에 따라 다른 일하기</button>
     </div>
   );
 }
