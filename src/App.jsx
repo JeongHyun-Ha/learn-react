7db297a4c61b6b149e5bcb73bf5010154dc52caa
JeongHyun Ha <aszx2024@naver.com>
2024-05-16 16:05:31 +0900

    응답 코드 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  const [code, setCode] = useState(200);
+  function handleClick200() {
+    axios.get("/api/main43/sub1").then((res) => console.log("200 응답"));
+  }
+
+  function handleClickCode() {
+    axios
+      .get(`/api/main43/sub3?code=${code}`)
+      .catch((res) => console.log(`${code} 응답`));
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick200}>200 응답</button>
+      <hr />
+      <input type="number" onChange={(e) => setCode(e.target.value)} />
+      <button onClick={handleClickCode}>{code} 응답</button>
+    </div>
+  );
 }
 
 export default App;
