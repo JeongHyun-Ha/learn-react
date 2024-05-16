3b2cf7bd199cd1e919b39f1e017002c503add02f
JeongHyun Ha <aszx2024@naver.com>
2024-05-16 12:06:18 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 function App(props) {
-  const [name, setName] = useState("");
-  const [city, setCity] = useState("");
-
-  const [country, setCountry] = useState("");
-  const [address, setAddress] = useState("");
-
-  function handleClick1() {
-    const obj1 = { name: "son", age: 44 };
-    // const data = JSON.stringify(obj1);
-    axios.post("/api/main41/sub1", obj1); // 자동으로 stringify 해줌
-  }
-
-  function handleClick2() {
-    const obj = { city: "seoul", country: "korea" };
-    axios.post("/api/main41/sub2", obj);
-  }
-
-  function handleClick3() {
-    // const data = {
-    //   name: name,
-    //   city: city,
-    // };
-    // const data = {
-    //   name,
-    //   city,
-    // };
-    axios.post("/api/main41/sub3", { name, city });
-  }
-
-  function handleClick4() {
-    axios.post("/api/main41/sub4", { country, address });
-  }
-
-  return (
-    <div>
-      <button onClick={handleClick1}>요청(json 데이터 포함)</button>
-      <button onClick={handleClick2}>요청(json 데이터 포함)2</button>
-      <div>
-        <input type="text" onChange={(e) => setName(e.target.value)} />
-        <br />
-        <input type="text" onChange={(e) => setCity(e.target.value)} />
-        <br />
-        <button onClick={handleClick3}>요청</button>
-      </div>
-      <hr />
-      <div>
-        <input type="text" onChange={(e) => setCountry(e.target.value)} />
-        <br />
-        <input type="text" onChange={(e) => setAddress(e.target.value)} />
-        <br />
-        <button onClick={handleClick4}>요청</button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
