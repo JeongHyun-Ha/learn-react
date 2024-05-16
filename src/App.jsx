a2d60ef220045d8a3398f85fce7a0154ae92cbd4
JeongHyun Ha <aszx2024@naver.com>
2024-05-16 14:03:11 +0900

    각 타입 데이터를 json 형식으로 보내기

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,28 @@ function App(props) {
     axios.post("/api/main41/sub5", data);
   }
 
+  function handleClick2() {
+    axios.post("/api/main41/sub6", {
+      name: "son",
+      age: 33,
+    });
+  }
+
+  function handleClick3() {
+    axios.post("/api/main41/sub7", {
+      name: "hello",
+      age: 100,
+    });
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>json 데이터와 요청</button>
+      <button onClick={handleClick2}>json 데이터와 요청</button>
+      <hr />
+      <div>
+        <button onClick={handleClick3}>json 데이터와 요청</button>
+      </div>
     </div>
   );
 }
