0a592c813efa084d45bc8704da7d64133c48c0c4
JeongHyun Ha <aszx2024@naver.com>
2024-05-16 11:41:34 +0900

    직접 입력한 데이터를 json 형식으로 보내기

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function App(props) {
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+
   function handleClick1() {
     const obj1 = { name: "son", age: 44 };
     // const data = JSON.stringify(obj1);
     axios.post("/api/main41/sub1", obj1); // 자동으로 stringify 해줌
   }
 
+  function handleClick2() {
+    const obj = { city: "seoul", country: "korea" };
+    axios.post("/api/main41/sub2", obj);
+  }
+
+  function handleClick3() {
+    // const data = {
+    //   name: name,
+    //   city: city,
+    // };
+    // const data = {
+    //   name,
+    //   city,
+    // };
+    axios.post("/api/main41/sub3", { name, city });
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>요청(json 데이터 포함)</button>
+      <button onClick={handleClick2}>요청(json 데이터 포함)2</button>
+      <div>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <br />
+        <input type="text" onChange={(e) => setCity(e.target.value)} />
+        <br />
+        <button onClick={handleClick3}>요청</button>
+      </div>
     </div>
   );
 }
