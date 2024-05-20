17fee9dddb85e38a6229a38c3cc128dd09dc0268
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 16:33:10 +0900

    map() 메소드에서 key props 사용하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  const names = ["ab", "cd", "ef", "gh"];
+
+  // 배열을 map 메소드를 사용해서 컴포넌트들을 만들 때
+  // key prop을 사용해서 효율성을 높일 것
+  // key prop 값은 주로 primary key가 사용됨
+  return (
+    <div>
+      {names.map((name, index) => (
+        <li key={index}>{name}</li>
+      ))}
+    </div>
+  );
 }
 
 export default App;
