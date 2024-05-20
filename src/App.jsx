dd8442c2a4ae79cd7c2646d0ff0e3b7d365e6df6
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 10:25:28 +0900

    Chakra UI
    sx: 스타일 덮어쓰기

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <Box bgColor={"orange"} _hover={{ bgColor: "yellow" }} cursor={"pointer"}>
-        Lorem ipsum dolor.
-      </Box>
+      <Button
+        sx={{ color: "orange", bg: "red", borderRadius: "30px" }}
+        colorScheme={"blue"}
+      >
+        Lorem.
+      </Button>
     </ChakraProvider>
   );
 }
