663e9417c2ae2dbd46d732bf238360ab55319152
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 10:36:48 +0900

    Chakra UI
    Flex, Center

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { Box, Center, ChakraProvider, Flex, Spacer } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <Button
-        sx={{ color: "orange", bg: "red", borderRadius: "30px" }}
-        colorScheme={"blue"}
-      >
-        Lorem.
-      </Button>
+      <Flex justify={"space-evenly"}>
+        <Box w={"100px"} bgColor={"blue"}>
+          1
+        </Box>
+        <Box w={"100px"} bgColor={"orange"}>
+          2
+        </Box>
+        <Box w={"100px"} bgColor={"yellow"}>
+          3
+        </Box>
+      </Flex>
+      <hr />
+      <Flex>
+        <Box bgColor={"gray"}>Lorem.</Box>
+        <Box bgColor={"gold"}>Nobis.</Box>
+        <Spacer />
+        <Box bgColor={"green"}>Necessitatibus.</Box>
+      </Flex>
+      <hr />
+      <Center>
+        <Box>Lorem.</Box>
+      </Center>
+      <hr />
+      <Center w={{ base: "50%", md: "600px" }} bgColor={"linen"}>
+        <Box>contents</Box>
+      </Center>
     </ChakraProvider>
   );
 }
