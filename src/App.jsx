b6cd8cb578efbe9c1afc28f5801b4f9fbd782c5c
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 09:25:34 +0900

    Chakra UI Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <div>
+        <Button colorScheme={"gray"}>Gray</Button>
+        <Button colorScheme={"red"}>Lorem.</Button>
+        <Button colorScheme={"orange"}>Sapiente.</Button>
+        <Button colorScheme={"yellow"}>Nemo.</Button>
+        <Button colorScheme={"green"}>Amet!</Button>
+        <Button colorScheme={"teal"}>Id!</Button>
+        <Button colorScheme={"blue"}>Quaerat.</Button>
+        <Button colorScheme={"cyan"}>Sequi.</Button>
+        <Button colorScheme={"purple"}>Doloribus.</Button>
+        <Button colorScheme={"pink"}>Quibusdam.</Button>
+        <hr />
+        <Button colorScheme={"red"} isLoading={true}>
+          Lorem.
+        </Button>
+        <hr />
+        <Button colorScheme={"red"} variant={"solid"}>
+          Lorem.
+        </Button>
+        <Button colorScheme={"red"} variant={"outline"}>
+          Deleniti?
+        </Button>
+        <Button colorScheme={"red"} variant={"ghost"}>
+          Nostrum.
+        </Button>
+        <Button colorScheme={"blue"} variant={"link"}>
+          Sed!
+        </Button>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
