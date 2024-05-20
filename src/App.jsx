e73d4b92f30d971b2ef5e8f9ff1d8844149e2039
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 12:26:20 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
-import { EmailIcon } from "@chakra-ui/icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faApple } from "@fortawesome/free-brands-svg-icons";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <Button leftIcon={<EmailIcon />}>Email</Button>
-      <FontAwesomeIcon icon={faApple} />
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
