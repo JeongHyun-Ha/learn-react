1bec827c62b8525acb2283a9a942e3d0e20579d4
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 12:35:17 +0900

    react hook(useXxx())은 컴포넌트 상단에 순서대로 작성, 조건/반복문 내에 사용 금지

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ChakraProvider, useDisclosure, useToast } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  // useXxx() : hook
+  const [count, setCount] = useState();
+  useEffect(() => {}, []);
+  const toast = useToast();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  // if / for 내에서 사용하지 말 것
+  /**
+   * if (true) {
+   *  const [text, setText] = useState("");
+   * }
+   */
+  // 안전한 사용 방법 : 컴포넌트 가장 상단에 순서대로 작성할 것
+
+  return (
+    <ChakraProvider>
+      <div></div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
