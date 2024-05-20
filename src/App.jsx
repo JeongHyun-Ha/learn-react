015032e92b17a3287061eba4b53e6dceb3fc4a08
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 11:10:36 +0900

    Chakra UI
    Form 컴포넌트들

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
 import React from "react";
 import {
+  Box,
   Center,
   ChakraProvider,
+  Checkbox,
   FormControl,
   FormHelperText,
   FormLabel,
   Input,
+  Radio,
+  RadioGroup,
+  Switch,
+  Textarea,
 } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <Center>
-        <FormControl>
-          <FormLabel>Email address</FormLabel>
-          <Input type="email" />
-          <FormHelperText>We will never share your email.</FormHelperText>
-        </FormControl>
-      </Center>
+      <FormControl>
+        <Center>
+          <Box>
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" />
+            <FormHelperText>We will never share your email.</FormHelperText>
+          </Box>
+        </Center>
+      </FormControl>
+      <hr />
+      <Checkbox>Lorem.</Checkbox>
+      <Checkbox>Id!</Checkbox>
+      <Checkbox>Quis?</Checkbox>
+      <hr />
+      <RadioGroup>
+        <Radio value="1">1</Radio>
+        <Radio value="2">2</Radio>
+        <Radio value="3">3</Radio>
+      </RadioGroup>
+      <hr />
+      <Switch />
+      <hr />
+      <Textarea></Textarea>
     </ChakraProvider>
   );
 }
