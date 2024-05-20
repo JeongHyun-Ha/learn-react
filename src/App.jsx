b91ba956a5ea67eabefc991eeab34f568f8c8d9f
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 14:28:36 +0900

    뱃지, stopPropagation()

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Badge, Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  function handleButtonClick(e) {
+    e.stopPropagation();
+    console.log("버튼 클릭됨");
+  }
+
+  function handleBoxClick() {
+    console.log("박스 클릭됨");
+  }
+
+  function handleBadgeClick(e) {
+    e.stopPropagation();
+    console.log("뱃지 클릭됨");
+  }
+
+  return (
+    <ChakraProvider>
+      <Box onClick={handleBoxClick} w={"100px"} h={"100px"} bgColor={"orange"}>
+        <Button onClick={handleButtonClick}>
+          클릭
+          <Badge onClick={handleBadgeClick} ml={3}>
+            new
+          </Badge>
+        </Button>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
