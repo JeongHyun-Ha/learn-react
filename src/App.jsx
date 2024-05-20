0d41592df6fd1b916612c77fac2be6156fd9e4a6
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 10:06:30 +0900

    Chakra UI
    스페이스

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,17 @@ import { Box, ChakraProvider } from "@chakra-ui/react";
 function App(props) {
   return (
     <ChakraProvider>
-      <Box p={"10px"} borderRadius={"md"} bg={"tomato"}>
+      <Box bgColor={"tomato"} m={1}>
         Lorem ipsum dolor.
       </Box>
-      <Box bgColor={"orange.800"}>Lorem ipsum dolor.</Box>
-      <hr />
-      <Box m={"10px"} bgColor={"yellow"}>
+      <Box bgColor={"tomato"} m={2}>
         Lorem ipsum dolor.
       </Box>
-      <Box mt={"10px"} bgColor={"yellow"}>
-        Molestias repudiandae, velit!
+      <Box bgColor={"tomato"} m={10}>
+        Ea, recusandae, sed.
       </Box>
-      <Box ml={"10px"} bgColor={"yellow"}>
-        At, culpa impedit.
-      </Box>
-      <hr />
-      <Box border={"1px solid red"} p={"10px"}>
-        Lorem ipsum dolor.
-      </Box>
-      <Box border={"1px solid red"} pt={"10px"}>
-        Aliquam, fugit, harum.
-      </Box>
-      <Box border={"1px solid red"} pb={"10px"}>
-        Corporis maxime, quisquam?
-      </Box>
-      <Box border={"1px solid red"} pl={"10px"}>
-        Ea nobis, similique.
-      </Box>
-      <Box border={"1px solid red"} pr={"10px"}>
-        Corporis, dignissimos tempore!
-      </Box>
-      <Box border={"1px solid red"} px={"10px"}>
-        Aliquam, eius quos!
-      </Box>
-      <Box border={"1px solid red"} py={"10px"}>
-        Delectus, dolorum, hic.
-      </Box>
-      <hr />
-      <Box w={"500px"} border={"1px solid red"}>
-        Lorem ipsum dolor.
-      </Box>
-      <Box h={"500px"} border={"1px solid red"}>
-        Possimus, sed temporibus?
+      <Box bgColor={"tomato"} m={20}>
+        Eaque, expedita, inventore!
       </Box>
     </ChakraProvider>
   );
