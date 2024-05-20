5586e96c8b897b946118b20e589bfffa04745766
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 14:12:24 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [text1, setText1] = useState("");
-  const [text2, setText2] = useState("hello");
-  const [text3, setText3] = useState("text3");
-  const [text4, setText4] = useState("text4");
-  const [text5, setText5] = useState("text5");
-
-  return (
-    <div>
-      <div>
-        <input type="text" onChange={(e) => setText1(e.target.value)} />
-        <span>{text1}</span>
-      </div>
-      <div>
-        <input type="text" value={text2} readOnly />
-      </div>
-      <div>
-        <input type="text" defaultValue={text3} />
-      </div>
-      <div>
-        <input
-          type="text"
-          value={text4}
-          onChange={(e) => setText4(e.target.value)}
-        />
-      </div>
-      <div>
-        <input type="text" value={text5} />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
