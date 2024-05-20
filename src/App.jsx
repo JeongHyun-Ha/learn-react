216a822d6cd2d4b4323b423823cdcc8eabe43bda
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 15:11:35 +0900

    파일을 보낼 때는 form 태그 encType 옵션을 'multipart/form-data'로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 
 function App(props) {
-  function handleLinkClick(e) {
-    e.preventDefault();
-    console.log("a 태그 클릭");
-  }
-
-  function handleSubmitForm(e) {
-    e.preventDefault();
-    console.log("폼 태그");
-  }
-
   return (
     <div>
-      <a href="https://www.google.com" onClick={handleLinkClick}>
-        구글
-      </a>
-      <hr />
-      <form action="https://www.google.com" onSubmit={handleSubmitForm}>
-        <input type="text" />
-        <button>전송</button>
+      <form action="/path" method="post" encType="multipart/form-data">
+        이름 <input type="text" name="name" />
+        <br />
+        <input type="file" name="file" />
+        <br />
+        <input type="submit" />
       </form>
     </div>
   );
