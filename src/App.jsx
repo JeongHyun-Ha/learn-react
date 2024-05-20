620c477212399431d49a8ff0bb474ae74f93dd44
JeongHyun Ha <aszx2024@naver.com>
2024-05-20 10:21:49 +0900

    Chakra UI
    hover

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import { Box, ChakraProvider } from "@chakra-ui/react";
 function App(props) {
   return (
     <ChakraProvider>
-      <Box bgColor={"tomato"} w={"500px"}>
-        Lorem ipsum dolor.
-      </Box>
-      <Box bgColor={"yellow.700"} w={{ base: "100%", md: "500px" }}>
+      <Box bgColor={"orange"} _hover={{ bgColor: "yellow" }} cursor={"pointer"}>
         Lorem ipsum dolor.
       </Box>
     </ChakraProvider>
